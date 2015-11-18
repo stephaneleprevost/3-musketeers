da8e7c64063b27a61ebf37669d4bf5610d8479d6
stephaneleprevost <stephane.leprevost@devinci.fr>
2015-11-18 11:47:12 +0100

    Finalisation des tests

--- test/index.js
+++ test/index.js
@@ -23,7 +23,7 @@ describe('is micro-checking library', function() {
       expect(is.array(['value1', 'value2'])).to.be.true;
     });
     it('should return false if passed argument type is not Array', function () {
-      expect(is.array('stephane')).to.be.false;
+      expect(is.array("stephane")).to.be.false;
 	  expect(is.date(123)).to.be.false;
 	  expect(is.date(true)).to.be.false;
 	  expect(is.date(null)).to.be.false;
@@ -36,7 +36,7 @@ describe('is micro-checking library', function() {
 	  expect(is.boolean(false)).to.be.true;
     });
 	it("should return false if passed parameter type is not boolean", function () {
-       expect(is.boolean('stephane')).to.be.false;
+       expect(is.boolean("stephane")).to.be.false;
 	   expect(is.boolean(123)).to.be.false;
 	   expect(is.boolean(null)).to.be.false;
     });
@@ -50,7 +50,7 @@ describe('is micro-checking library', function() {
     });
 	
 	it("should return false if passed parameter type is not date", function () {
-      expect(is.date('stephane')).to.be.false;
+      expect(is.date("stephane")).to.be.false;
 	  expect(is.date(123)).to.be.false;
 	  expect(is.date(true)).to.be.false;
 	  expect(is.date(null)).to.be.false;
@@ -67,7 +67,7 @@ describe('is micro-checking library', function() {
     });
 	
     it("should return false if passed parameter type is not error", function () {
-      expect(is.date('stephane')).to.be.false;
+      expect(is.date("stephane")).to.be.false;
 	  expect(is.date(123)).to.be.false;
 	  expect(is.date(true)).to.be.false;
 	  expect(is.date(null)).to.be.false;
@@ -83,7 +83,7 @@ describe('is micro-checking library', function() {
     });
 	
     it("should return false if passed parameter type is not function", function () {
-      expect(is.date('stephane')).to.be.false;
+      expect(is.date("stephane")).to.be.false;
 	  expect(is.date(123)).to.be.false;
 	  expect(is.date(true)).to.be.false;
 	  expect(is.date(null)).to.be.false;
@@ -96,7 +96,7 @@ describe('is micro-checking library', function() {
 	  expect(is.nan(0/0)).to.be.true;
     });
     it("should return false if passed parameter type is not Nan", function () {
-      expect(is.date('stephane')).to.be.false;
+      expect(is.date("stephane")).to.be.false;
 	  expect(is.date(123)).to.be.false;
 	  expect(is.date(true)).to.be.false;
 	  expect(is.date(null)).to.be.false;
@@ -109,7 +109,7 @@ describe('is micro-checking library', function() {
 	  expect(is.null(null)).to.be.true;
     });
     it("should return false if passed parameter type is not null", function () {
-      expect(is.null('stephane')).to.be.false;
+      expect(is.null("stephane")).to.be.false;
 	  expect(is.null(123)).to.be.false;
       expect(is.null(true)).to.be.false;
     });
@@ -121,7 +121,7 @@ describe('is micro-checking library', function() {
 	  expect(is.number(123)).to.be.true;
     });
     it("should return false if passed parameter type is not number", function () {
-      expect(is.date('stephane')).to.be.false;
+      expect(is.date("stephane")).to.be.false;
 	  expect(is.date(true)).to.be.false;
 	  expect(is.date(null)).to.be.false;
     });
@@ -133,10 +133,92 @@ describe('is micro-checking library', function() {
 	  expect(is.object(new Object())).to.be.true;
     });
     it("should return false if passed parameter type is not object", function () {
-	  expect(is.null('stephane')).to.be.false;
+	  expect(is.null("stephane")).to.be.false;
 	  expect(is.null(123)).to.be.false;
       expect(is.null(true)).to.be.false;
     });
   });
+  
+  describe('is.json', function () {
+
+    it('should return true if passed parameter type is JSON object', function () {
+	  expect(is.json({foo: 'bar'})).to.be.true;
+	  expect(is.any.json({}, 2)).to.be.true;
+	  expect(is.any.json([{}, {foo: 'bar'}]).to.be.true;
+    });
+    it("should return false if passed parameter type is not JSON object", function () {
+	  expect(is.json("stephane")).to.be.false;
+	  expect(is.json(123)).to.be.false;
+	  expect(is.date(null)).to.be.false;
+	  expect(is.null(true)).to.be.false;
+    });
+  });
+
+ describe('is.regexp', function () {
+
+    it('should return true if passed parameter type is regExp', function () {
+	  expect(is.regexp(new RegExp('/^[a-z0-9-]+$/'))).to.be.true;
+    });
+    it("should return false if passed parameter type is not regExp", function () {
+	  expect(is.regexp("stephane")).to.be.false;
+	  expect(is.regexp(123)).to.be.false;
+	  expect(is.date(null)).to.be.false;
+	  expect(is.null(true)).to.be.false;
+    });
+  });
+  
+   describe('is.string', function () {
+
+    it('should return true if passed parameter type is string', function () {
+	  expect(is.string("stephane")).to.be.true;
+    });
+    it("should return false if passed parameter type is not string", function () {
+	  expect(is.string(123)).to.be.false;
+	  expect(is.date(null)).to.be.false;
+	  expect(is.string(true)).to.be.false;
+    });
+  });
+
+ describe('is.char', function () {
+
+    it('should return true if passed parameter type is string', function () {
+	  expect(is.char('S')).to.be.true;
+    });
+    it("should return false if passed parameter type is not string", function () {
+	  expect(is.char("stephane")).to.be.false;
+	  expect(is.char(123)).to.be.false;
+	  expect(is.date(null)).to.be.false;
+	  expect(is.char(true)).to.be.false;
+    });
+  });
+
+ describe('is.undefined', function () {
+
+    it('should return true if passed parameter type is string', function () {
+	  expect(is.undefined(undefined)).to.be.true;
+    });
+    it("should return false if passed parameter type is not string", function () {
+	  expect(is.char("stephane")).to.be.false;
+	  expect(is.char(123)).to.be.false;
+	  expect(is.date(null)).to.be.false;
+	  expect(is.char(true)).to.be.false;
+    });
+  });
+  
+  describe('is.sameType', function () {
+
+    it('should return true if passed parameter type are same type', function () {
+	  expect(is.sameType("stephane","leprevost")).to.be.true;
+	  expect(is.sameType(1,2)).to.be.true;
+	  expect(is.sameType(true,false)).to.be.true;
+    });
+    it("should return false if passed parameter type are not same type", function () {
+	  expect(is.sameType("stephane",123)).to.be.false;
+	  expect(is.sameType(true,null)).to.be.false;
+	  expect(is.sameType('S',"stephane")).to.be.false;
+	  expect(is.sameType(false,123)).to.be.false;	  
+    });
+  });
+
 
 
